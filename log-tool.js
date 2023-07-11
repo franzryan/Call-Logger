12f7945b7348df5b07183ef0c7d344fa2f18a2d2
Franz Ryan <franz.ryan12@gmail.com>
2023-07-11 17:02:08 +0800

    Add files via upload

--- log-tool.js
+++ log-tool.js
@@ -1,17 +1,14 @@
-
-//toggles between IPA and NON-IPA 
-let displayIPA = document.getElementById("call-two-ipa-id");
-let displayMEM = document.getElementById("call-two-non-ipa-id");
-let ipaRadio = document.getElementById("ipa-radio");
-let memRadio = document.getElementById("mem-radio");
-
-function turnDisplayIpa() {
-    displayIPA.style.display = "";
-    displayMEM.style.display = "none";
-};
-function turnDisplayMem() {
-    displayIPA.style.display = "none";
-    displayMEM.style.display = "block";
+let displayIpa = document.getElementById("ipa-radio");
+let displayMem = document.getElementById("mem-radio");
+let ipaWindow = document.getElementById("ipa-window");
+let memWindow = document.getElementById("mem-window");
+function changeDisplayIpa() {
+    ipaWindow.style.display = "";
+    memWindow.style.display = "none";
 }
-ipaRadio.addEventListener("focus", turnDisplayIpa);
-memRadio.addEventListener("focus", turnDisplayMem);
+function changeDisplayMem() {
+    ipaWindow.style.display = "none";
+    memWindow.style.display = "block";
+}
+displayIpa.addEventListener("focus", changeDisplayIpa)
+displayMem.addEventListener("focus", changeDisplayMem)

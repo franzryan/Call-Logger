8645a691690eb5a1859764053aff6fae89a98d9c
Franz Ryan <franz.ryan12@gmail.com>
2023-07-12 16:51:33 +0800

    Add files via upload

--- log-tool.js
+++ log-tool.js
@@ -2,13 +2,12 @@ let displayIpa = document.getElementById("ipa-radio");
 let displayMem = document.getElementById("mem-radio");
 let ipaWindow = document.getElementById("ipa-window");
 let memWindow = document.getElementById("mem-window");
-function changeDisplayIpa() {
+displayIpa.addEventListener("focus", () => {
     ipaWindow.style.display = "";
     memWindow.style.display = "none";
-}
-function changeDisplayMem() {
+  });
+  
+  displayMem.addEventListener("focus", () => {
     ipaWindow.style.display = "none";
     memWindow.style.display = "block";
-}
-displayIpa.addEventListener("focus", changeDisplayIpa)
-displayMem.addEventListener("focus", changeDisplayMem)
+  });

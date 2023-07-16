5e92b42a399bb68b4ec23a648fe7b8590d69b6f1
Franz Ryan <franz.ryan12@gmail.com>
2023-07-16 23:50:36 +0800

    Add files via upload

--- log-tool.js
+++ log-tool.js
@@ -1,3 +1,5 @@
+// Call Log Section
+
 let displayIpa = document.getElementById("ipa-radio");
 let displayMem = document.getElementById("mem-radio");
 let ipaWindow = document.getElementById("ipa-window");
@@ -11,3 +13,22 @@ displayIpa.addEventListener("focus", () => {
     ipaWindow.style.display = "none";
     memWindow.style.display = "block";
   });
+
+  let attachInputs = document.getElementsByClassName("call-inputs");
+  let attachHere = document.getElementById("call-log-textarea");
+  let inputLabels = document.getElementsByClassName("input-label");
+  
+  function attachLogs(inputLabels, attachInputs) {
+    let inputTexts = attachHere.value; // Initialize with current textarea value
+    for (let i = 0; i < inputLabel.length && i < attachInputs.length; i++) {
+      if (attachInputs[i].type === "radio" && !attachInputs[i].checked) {
+        continue; // Skip radio buttons that are not selected
+      }
+      inputTexts += inputLabel[i].innerHTML + attachInputs[i].value + "\n";
+    }
+  
+    attachHere.value = inputTexts; //
+  }
+
+
+  // call log section

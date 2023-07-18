708756a9b8f11a66f25fd4213cccc8cfcb5cad66
Franz Ryan <franz.ryan12@gmail.com>
2023-07-18 17:11:23 +0800

    Add files via upload

--- log-tool.js
+++ log-tool.js
@@ -17,12 +17,28 @@ displayIpa.addEventListener("focus", () => {
 let firstLabels = document.getElementsByClassName("input-label1");
 let firstInputs = document.getElementsByClassName("call-inputs1");
 let attachTexts = document.getElementById("call-log-textarea1");
+let lastRadio = document.getElementsByClassName("last-radio-line");
 
 function attachLogs() {
-  let firstInputsAttached = "";
-for (i = 0; i < firstLabels.length; i++) {
-  firstInputsAttached += firstLabels[i].textContent + ":" + "\n";
+  let inputsAttached = "";
+for (i = 0; i < firstInputs.length; i++) {
+  if (firstInputs[i].value === "") {
+    continue;
+  }
+  else if (firstInputs[i].type === "radio" && !firstInputs[i].checked) {
+    continue;
+  }
+  inputsAttached += firstLabels[i].textContent + i + ":" + "\n" + "\t" + firstInputs[i].value + "\n";
 }
-attachTexts.value = firstInputsAttached;
-}
+attachTexts.value = inputsAttached;
+};
   // call log section
+
+  // let inputsRadioAttached = "";
+  // for (i = 0; i < callRadioBtn.length; i++) {
+  //   if (!callRadioBtn.checked) {
+  //     continue;
+  //     }
+  //     inputsRadioAttached += labelRadioBtn[i].textContent + callRadioBtn[i].value;
+  // }
+  // attachTexts.value = inputsRadioAttached;

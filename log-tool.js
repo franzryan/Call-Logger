8ffdbb524825a30e72f7019450dd550db6550d33
Franz Ryan <franz.ryan12@gmail.com>
2023-07-20 16:46:59 +0800

    Add files via upload

--- log-tool.js
+++ log-tool.js
@@ -17,25 +17,37 @@ displayIpa.addEventListener("focus", () => {
 let firstLabels = document.getElementsByClassName("input-label1");
 let firstInputs = document.getElementsByClassName("call-inputs1");
 let attachTexts = document.getElementById("call-log-textarea1");
+let attachTextsSecond = document.getElementById("call-log-textarea2");
+let lastLabel = document.getElementById("last-call-label");
+let lastInput = document.getElementById("last-call-input");
 
 function attachLogs() {
   let inputsAttached = "";
 for (i = 0; i < firstInputs.length; i++) {
   if (firstInputs[i].value === "") {
-    continue;
-  }
-  if (firstInputs[i].type === "radio") {
+    continue;}
+  else if (firstInputs[i].type === "radio") {
     if (firstInputs[i].checked) {
-      inputsAttached += firstInputs[i].value + i + "\n";
-      continue;
-    }
-  }
+      inputsAttached += firstInputs[i].name + ": " + firstInputs[i].value + "\n";
+    }}
   else {
-    inputsAttached += firstLabels[i].textContent + ":" + "\n" + "\t" 
-    + firstInputs[i].value + " " + i + "\n";
+    inputsAttached += firstLabels[i].textContent + ": "
+    + firstInputs[i].value + "\n";}
   }
-}
+  if (lastInput.value === "") {
+    includeLastInput = false;
+  } else {
+    inputsAttached += lastLabel.textContent + ":\n" + lastInput.value;
+  }
 attachTexts.value = inputsAttached;
 };
+
+function copyLogs() {
+  let attachBothLogs = "";
+  attachBothLogs += attachTexts.value + attachTextsSecond.value + "\n";
+  navigator.clipboard.writeText(attachBothLogs)
+ 
+}
+
 
   // call log section
